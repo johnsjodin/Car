53bf6fd510d93afc3b7f925d4560adfba5d8cfbf
John Sjödin <sjodin.j@gmail.com>
2025-09-18 10:17:07 +0200

    Add Car class, create instance of Car in Main.

---
+++ Car.cs
@@ -0,0 +1,12 @@
+﻿namespace Car
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Color;
+        public int Year;
+        public int HorsePower;
+        public int Gears;
+        public bool EngineOn;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            
+            myCar.Brand = "Tesla";
+            myCar.Color = "Black";
+            myCar.Year = 2025;
+            myCar.HorsePower = 1200;
+            myCar.Gears = 5;
+            myCar.EngineOn = true;
+
+            Console.WriteLine($"Märke: {myCar.Brand}");
+            Console.WriteLine($"Färg: {myCar.Color}");
+            Console.WriteLine($"Årsmodell: {myCar.Year}");
+            Console.WriteLine($"Hästkrafter: {myCar.HorsePower}");
+            Console.WriteLine($"Växlar: {myCar.Gears}");
+            Console.WriteLine($"Motor igång: {myCar.EngineOn}");
+
+
         }
     }
 }
